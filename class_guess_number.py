32e45f41cb33d813e94469f9c5d50f0d3638bae3
rvxt21 <anastasia1koryagina@gmail.com>
2025-12-15 21:18:08 +0200

    add attempt number to the game

--- class_guess_number.py
+++ class_guess_number.py
@@ -4,9 +4,10 @@ class GuessNumberGame:
     number_to_guess: int
 
     def __init__(self):
-        self.number_to_guess = 0
-        self.user_input = 0
-        self.player_guessed = False
+        self.number_to_guess: int  = 0
+        self.user_input: int = 0
+        self.player_guessed: bool = False
+        self.attempt: int = 1
 
     def generate_number(self):
         self.number_to_guess = random.randint(1,10)
@@ -23,18 +24,25 @@ class GuessNumberGame:
     def check_user_input(self):
         self.player_guessed = self.user_input == self.number_to_guess
 
-    def get_next_input_hint(self):
+    def get_next_input_hint(self) -> str:
         if self.user_input > self.number_to_guess:
-                return "Try smaller number: "
+            return "Try smaller number: "
         else:
             return "Try higher number: "
 
+    def print_attempt_number_hint(self) -> str:
+         print(f"It`s your {self.attempt} attempt.")
+    
+    def increase_attempt_number(self) -> int:
+        self.attempt += 1 
     
     def play(self):
         self.generate_number()
         print(self.number_to_guess)
         self.get_user_input("Guess the number from 1 to 10: ")
         while not self.player_guessed:
+            self.increase_attempt_number()
+            self.print_attempt_number_hint()
             hint: str = self.get_next_input_hint()
             self.get_user_input(hint)
             self.check_user_input()
